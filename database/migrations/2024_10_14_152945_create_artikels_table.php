5ef6b2dd02cddfffd3b098808d41cebb7cf29d1e
Jason-77777 <jason.fan2828@gmail.com>
2024-10-16 21:18:34 +0700

    Update 2024_10_14_152945_create_artikels_table.php

--- database/migrations/2024_10_14_152945_create_artikels_table.php
+++ database/migrations/2024_10_14_152945_create_artikels_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('artikels', function (Blueprint $table) {
             $table->id('artikelID');
             $table->char('judul', 50);
-            $table->text('artikel', 250);
+            $table->string('artikel', 250);
             $table->timestamps();
         });
     }
