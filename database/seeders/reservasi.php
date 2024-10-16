f2f40b908f62243f68d40ca03c3a93b9343a50dc
VickyDarmana <134504626+VickyDarmana@users.noreply.github.com>
2024-10-16 20:52:23 +0700

    Update reservasi.php

---
+++ database/seeders/reservasi.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            [
+                'userID' => '0001',
+                'name' => 'Andi',
+                'tanggal' => Carbon::now(),
+                'waktu' => Carbon::now(),
+                'meja' => '2',
+                'jumlah_pax' => '10',
+                'status' => 'Reservasi Diterima',
+            ],
+
+            [
+                'userID' => '0002',
+                'name' => 'Susan',
+                'tanggal' => Carbon::now(),
+                'waktu' => Carbon::now(),
+                'meja' => '1',
+                'jumlah_pax' => '5',
+                'status' => 'Reservasi Diterima',
+            ],
+
+            [
+                'userID' => '0003',
+                'name' => 'Michael',
+                'tanggal' => Carbon::now(),
+                'waktu' => Carbon::now(),
+                'meja' => '3',
+                'jumlah_pax' => '15',
+                'status' => 'Reservasi Ditolak',
+            ],
+
+        
+        ]);
+    }
+}
