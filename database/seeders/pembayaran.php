28592d7509723ab386af8aea6a175a37b2234157
VickyDarmana <134504626+VickyDarmana@users.noreply.github.com>
2024-10-17 20:01:54 +0700

    update

--- database/seeders/pembayaran.php
+++ database/seeders/pembayaran.php
@@ -3,20 +3,15 @@
 namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory as Faker;
+use App\Models\pembayaran;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
             [
